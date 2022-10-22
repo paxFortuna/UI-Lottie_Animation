9b3c469af06c7bc86255aa30cc5129bd4f69db0d
fortuna <avidia38@gmail.com>
2022-10-22 16:35:26 +0900

    feat: lottie & animation_controller

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,7 +8,30 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  // controller
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  bool pumpkiRun = false;
+  bool bookMarked = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,12 +41,40 @@ class _HomePageState extends State<HomePage> {
       body: Center(
         child: Column(
           children: [
-            Lottie.network('https://assets3.lottiefiles.com/packages/lf20_6FP3kYFVYd.json'),
+            GestureDetector(
+              onTap: () {
+                if (pumpkiRun) {
+                  pumpkiRun = true;
+                  _controller.forward();
+                } else {
+                  _controller.reverse();
+                  pumpkiRun = false;
+                }
+              },
+              child: Lottie.network(
+                'https://assets3.lottiefiles.com/packages/lf20_6FP3kYFVYd.json',
+                controller: _controller,
+              ),
+            ),
             const SizedBox(height: 20),
-            Lottie.network('https://assets8.lottiefiles.com/packages/lf20_xdfeea13.json')
+            GestureDetector(
+                onTap: () {
+                  if (bookMarked == false) {
+                    bookMarked = true;
+                    _controller.forward();
+                  } else {
+                    _controller.reverse();
+                    bookMarked = false;
+                  }
+                },
+                child: Lottie.network(
+                  'https://assets8.lottiefiles.com/packages/lf20_xdfeea13.json',
+                  controller: _controller,
+                ))
           ],
         ),
       ),
-    );;
+    );
+    ;
   }
 }
